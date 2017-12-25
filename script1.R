3d59e091ca1979e96d5938aeef83dfc09404ae71
paulofloliveira <paulofloliveira.adv@gmail.com>
2017-12-25 14:40:15 -0300

    Primeira modificação no Script 1 para a inclusão do Artigo 1 da TESE - Aplicações do Controle Sintético no Direito

--- script1.R
+++ script1.R
@@ -1,1 +1,15 @@
-#Paulo Fernando de Lima Oliveira
+#########################################
+#Universidade Federal de Pernambuco     #
+#Centro de Ciências Jurídicas           #
+#Doutorado em Direito                   #
+#Teoria da Decisão Judicial             #
+#                                       #
+#Paulo Fernando de Lima Oliveira        #
+#########################################
+
+#Artigo I - Synthetic Control application in law research
+
+
+library ('tidyverse')
+library ('Synth')
+
