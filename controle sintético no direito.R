ee4ada4c7da8937b424ea5081afdac3d29d885a1
paulofloliveira <paulofloliveira.adv@gmail.com>
2018-01-06 13:07:50 -0300

    segundo commit

---
+++ controle sintético no direito.R
@@ -0,0 +1,19 @@
+#########################################
+#Universidade Federal de Pernambuco     #
+#Centro de Ciências Jurídicas           #
+#Doutorado em Direito                   #
+#Teoria da Decisão Judicial             #
+#                                       #
+#Paulo Fernando de Lima Oliveira        #
+#########################################
+
+#Artigo I - Synthetic Control application in law research
+
+#Controle Sintético
+
+#Linguagem de programação: R
+#Biblioteca: Synth
+
+library ('tidyverse')
+library ('Synth')
+
